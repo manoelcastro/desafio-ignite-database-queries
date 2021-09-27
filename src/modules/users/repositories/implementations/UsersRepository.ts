760d65cd8cb4ef11c59c35a89cc1dbc68b87e4e9
manoelcastro <manoelcastroadm@gmail.com>
2021-09-27 18:57:20 -0300

    Mais Um teste

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -27,8 +27,6 @@ export class UsersRepository implements IUsersRepository {
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    const first = first_name.toLowerCase();
-    const last = last_name.toLowerCase();
-    return this.repository.query(`SELECT * FROM users WHEN first_name = ${first} AND last_name = ${last}`); // Complete usando raw query
+    return this.repository.query('SELECT * FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = LOWER($2)', [first_name, last_name]); // Complete usando raw query
   }
 }
